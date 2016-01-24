62314417f156c4747dda470e454329f012e4bb44
mz2 <matias.piipari@gmail.com>
2016-01-24 20:24:10 +0000

    Adds an .xcodeproj

---
+++ RMSkinnedView/RMSkinnedViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  RMSkinnedView.h
+//  RMSkinnedView
+//
+//  Created by Matias Piipari on 24/01/2016.
+//  Copyright © 2016 Manuscripts.app Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RMSkinnedView.
+FOUNDATION_EXPORT double RMSkinnedViewVersionNumber;
+
+//! Project version string for RMSkinnedView.
+FOUNDATION_EXPORT const unsigned char RMSkinnedViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMSkinnedView/PublicHeader.h>
+
+#import "RMSkinnedView.h"
